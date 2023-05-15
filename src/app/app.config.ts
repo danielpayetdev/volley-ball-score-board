73430a7b606a602ab8a9fab79ee45737aa7541d3
Daniel Payet <danieldu974@gmail.com>
2023-05-15 19:26:53 +0200

    add withHashLocation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { withHashLocation } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [provideRouter(routes, withHashLocation())]
 };
